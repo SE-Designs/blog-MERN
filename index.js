e59c1d684eab6ffe02972658d221dd4a0ddc6a1b
mixture <dragonlorefnstattrack@gmail.com>
2023-05-09 13:01:26 +0600

    add mongodb, jwt

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+	.connect(
+		`mongodb+srv://mix:<password>@mern-blog.mmgvmgk.mongodb.net/?retryWrites=true&w=majority`
+	)
+	.then(() => console.log("DB ok"))
+	.catch((err) => console.error("DB err: ", err));
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
 	res.send("Hello World");
+});
+
+app.post("/auth/login", (req, res) => {
+	console.log(req.body);
+
+	const token = jwt.sign(
+		{
+			email: req.body.email,
+			fullName: "mixturegg",
+		},
+		"iamsecretkey"
+	);
+
+	res.json({
+		login: "mixture",
+		success: true,
+	});
 });
 
 app.listen(3000, (err) => {
