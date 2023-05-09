20a86fa64e4d5200daad36b8a4338074fca9ae68
mixture <dragonlorefnstattrack@gmail.com>
2023-05-09 19:03:52 +0600

    add handle validations errors, add multer, refactoring code

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,9 @@
+import UserModel from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 export const signUp = async (req, res) => {
 	try {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json(errors.array());
-		}
-
 		const password = req.body.password;
 		const salt = await bcrypt.genSalt(10);
 		const hash = await bcrypt.hash(password, salt);
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-import * as PostController from "./controllers/PostController.js";
-import * as UserController from "./controllers/UserController.js";
 import * as dotenv from "dotenv";
 
+import { PostController, UserController } from "./controllers/index.js";
 import { loginValidation, registerValidation } from "./validations/auth.js";
 
-import UserModel from "./models/User.js";
-import bcrypt from "bcrypt";
 import checkAuth from "./utils/checkAuth.js";
 import express from "express";
-import jwt from "jsonwebtoken";
+import handleValErrors from "./utils/handleValErrors.js";
 import mongoose from "mongoose";
+import multer from "multer";
 import { postCreateValidation } from "./validations/post.js";
-import { validationResult } from "express-validator";
 
 dotenv.config();
 
@@ -24,22 +21,84 @@ mongoose
 
 const app = express();
 
-app.use(express.json());
+const storage = multer.diskStorage({
+	destination: (_, __, cb) => {
+		cb(null, "images");
+	},
+	filename: (_, file, cb) => {
+		cb(null, file.originalname);
+	},
+});
 
-app.post("/auth/sign-in", loginValidation, UserController.signUp);
-app.post("/auth/sign-up", registerValidation, UserController.signIn);
+const fileFilter = (_, file, cb) => {
+	if (
+		file.mimetype === "image/png" ||
+		file.mimetype === "image/jpg" ||
+		file.mimetype === "image/jpeg"
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
+const upload = multer({ storage, fileFilter });
+
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.post(
+	"/auth/sign-up",
+	registerValidation,
+	handleValErrors,
+	UserController.signUp
+);
+app.post(
+	"/auth/sign-in",
+	loginValidation,
+	handleValErrors,
+	UserController.signIn
+);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+	res.json({
+		url: `/uploads/${req.file.originalname}`,
+	});
+});
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
-app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.post(
+	"/posts",
+	checkAuth,
+	postCreateValidation,
+	handleValErrors,
+	PostController.create
+);
 app.delete("/posts/:id", checkAuth, PostController.remove);
-app.patch("/posts/:id", checkAuth, PostController.update);
+app.patch(
+	"/posts/:id",
+	checkAuth,
+	postCreateValidation,
+	handleValErrors,
+	PostController.update
+);
 
-app.listen(3000, (err) => {
+app.use((error, req, res, next) => {
+	const status = error.errorStatus || 500;
+	const message = error.message;
+	const data = error.data;
+	console.log(message, status, error);
+	res.status(status).json({ message: message, data: data });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (err) => {
 	if (err) {
 		return console.error(err);
 	}
 
-	console.log("starting server at port 3000");
+	console.log(`Server running on port - ${PORT}`);
 });
